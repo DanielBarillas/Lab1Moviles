b0fdd550e9f63d025c8ea214a64ad48218555936
Daniel Barillas <98109214+DanielBarillas@users.noreply.github.com>
2023-07-16 22:06:08 -0600

    Versión final
    Versión final del programa desarrollado en Java de la parte 1 del laboratorio 1

---
+++ Parte 1_Lab 1_Móviles_Java_Versión final/ItemDataJ.java
@@ -0,0 +1,81 @@
+/**
+ * <h1> Laboratorio 1 </h1>
+ * <h2> Main (Clase principal) </h2>
+ * 
+ * <p>
+ * Programación de plataformas móviles - Universidad del Valle de Guatemala
+ * </p>
+ * 
+ * Creado por:
+ * 
+ * @author Pablo Daniel Barillas Moreno || Carné No. 22193
+ * @version 1.0
+ * @since 16/07/2023
+ * 
+ **/ 
+
+import javax.swing.JOptionPane;
+
+public class ItemDataJ {
+    private Object originalValue;
+
+    public ItemDataJ(Object originalValue) {
+        this.originalValue = originalValue;
+    }
+
+    public static void main(String[] args) {
+        String inputValue = JOptionPane.showInputDialog("Ingrese el dato que desee:");
+        Object originalValue;
+        try {
+            originalValue = Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            originalValue = inputValue;
+        }
+        ItemDataJ data = new ItemDataJ(originalValue);
+        data.showInfo();
+    }
+
+    public String getType() {
+        if (originalValue instanceof String) {
+            return "cadena";
+        } else if (originalValue instanceof Integer) {
+            return "entero";
+        } else if (originalValue instanceof Boolean) {
+            return "booleano";
+        } else {
+            return null;
+        }
+    }
+
+    public String getInfo() {
+        if (originalValue instanceof String) {
+            String cadena = (String) originalValue;
+            return "L" + cadena.length();
+        } else if (originalValue instanceof Integer) {
+            int valor = (Integer) originalValue;
+            if (valor % 10 == 0) {
+                return "M10";
+            } else if (valor % 5 == 0) {
+                return "M5";
+            } else if (valor % 2 == 0) {
+                return "M2";
+            } else {
+                return null;
+            }
+        } else if (originalValue instanceof Boolean) {
+            boolean valor = (Boolean) originalValue;
+            return valor ? "Verdadero" : "Falso";
+        } else {
+            return null;
+        }
+    }
+
+    public void showInfo() {
+        String type = getType();
+        String info = getInfo();
+        String message = "El dato ingresado es: " + originalValue + "\n"
+                + "El tipo de la variable ingresada es: " + type + "\n"
+                + "Información del valor: " + (info != null ? info : "No aplica");
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
